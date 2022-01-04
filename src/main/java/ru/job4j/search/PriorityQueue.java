9b00762255275a1f55d935f467c1ba03e8237d37
AntonMel2 <Motna9@gmail.com>
2022-01-04 16:18:16 +0300

    2. Очередь с приоритетом на LinkedList [#41670 #246831]

--- src/main/java/ru/job4j/search/PriorityQueue.java
+++ src/main/java/ru/job4j/search/PriorityQueue.java
@@ -8,11 +8,11 @@ public class PriorityQueue {
     public void put(Task task) {
         int index = 0;
         for (Task element : tasks) {
-            if (element.getPriority() <= task.getPriority()) {
-                index++;
-            } else {
+            if (element.getPriority() > task.getPriority()) {
                 tasks.add(index, task);
                 break;
+            } else {
+                index++;
             }
         }
         this.tasks.add(index, task);
