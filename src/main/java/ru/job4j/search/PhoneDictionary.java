345f1cba1da456409c62330f2c9174518fab6ebf
AntonMel2 <Motna9@gmail.com>
2022-01-05 14:21:40 +0300

    1. Телефонный справочник на базе ArrayList [#41598 #246892]

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -11,20 +11,12 @@ public class PhoneDictionary {
 
     public ArrayList<Person> find(String key) {
         ArrayList<Person> result = new ArrayList<>();
-        for (int i = 0; i < result.size(); i++) {
-            if (result.get(i).getName().contains(key) || result.get(i).getSurname().contains(key) || result.get(i).getPhone().contains(key) || result.get(i).getAddress().contains(key)) {
-                return result;
+        for (Person num : persons) {
+            if (num.getName().contains(key) || num.getSurname().contains(key)
+                    || num.getPhone().contains(key) || num.getAddress().contains(key)) {
+                result.add(num);
             }
         }
         return result;
-    }
-
-    public static void main(String[] args) {
-        PhoneDictionary phones = new PhoneDictionary();
-        phones.add(
-                new Person("Petr", "Arsentev", "534872", "Bryansk")
-        );
-        ArrayList<Person> persons = phones.find("Petr");
-        System.out.println(phones);
     }
 }
