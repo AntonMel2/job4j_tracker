f82afff430f4d62f5f8cc4fde40171bd29a67595
AntonMel2 <Motna9@gmail.com>
2022-01-19 21:18:53 +0300

    2. Идентичные тексты [#212655]

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,23 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+    public static boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> chek = new HashSet<>();
+        for (String ornum : origin) {
+            chek.add(ornum);
+        }
+        for (String rs : text) {
+            if (!chek.contains(rs)) {
+                rsl = false;
+            }
+        }
+        return rsl;
+    }
+}
+
+
